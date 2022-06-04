260354277a12801ab11d76de7a6a340a9f6537f6
maialele <mayalapi@gmail.com>
2022-06-04 18:51:17 +0300

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(num1) {
+    fibarray = [0,1]
+    for (let i = 1; i <= num1; i++ ) {
+        fibarray.push(fibarray[i] + fibarray[i - 1]);
+    }
+    return fibarray[num1];
 };
 
 // Do not edit below this line
