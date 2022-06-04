916fbf4dba68bb354434daa22808736a5bcfe20e
maialele <75091525+maialele@users.noreply.github.com>
2022-06-04 18:58:26 +0300

    Update fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,9 +1,9 @@
-const fibonacci = function(num1) {
+const fibonacci = function(num) {
     fibarray = [0,1]
-    for (let i = 1; i <= num1; i++ ) {
+    for (let i = 1; i <= num; i++ ) {
         fibarray.push(fibarray[i] + fibarray[i - 1]);
     }
-    return fibarray[num1];
+    return fibarray[num];
 };
 
 // Do not edit below this line
